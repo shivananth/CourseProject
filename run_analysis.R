2368a9403b369be8bdb3b8e2aa115103e5bedade
shivananth <shivananth4u@gmail.com>
2014-05-24 14:57:02 +0530

    updating the code and dataset for changes in column names

--- run_analysis.R
+++ run_analysis.R
@@ -72,31 +72,54 @@ name.part2<-unlist(strsplit(as.character(features.name.split.cleaned$measurement
 name.part3<-features.name.split.cleaned$measuredRange[features.index]
 
 feature.names<-ifelse(is.na(name.part3)==FALSE,
-                      paste(name.part1,name.part2,name.part3,sep="."),
-                      paste(name.part1,name.part2,sep="."))
+                      paste(name.part1,name.part3,name.part2,sep=""),
+                      paste(name.part1,name.part2,sep=""))
+
+##removing the word "Body" when it appears twi
+feature.names.v1<-gsub("([Bb][Oo][Dd][Yy])\\1","Body",feature.names)
+
+##changing t to timedomain; f to frequencydomain;
+feature.names.v2<-gsub("^t","timeDomain",feature.names.v1) 
+feature.names.v3<-gsub("^f","frequencyDomain",feature.names.v2) 
+
+##cleaning for acc, gyro, axis in variable names
+feature.names.v4<-gsub("Acc","Accelerometer",feature.names.v3) 
+feature.names.v5<-gsub("Gyro","Gyroscope",feature.names.v4) 
+feature.names.v6<-gsub("[X]","Xaxis",feature.names.v5) 
+feature.names.v7<-gsub("[Y]","Yaxis",feature.names.v6) 
+feature.names.v8<-gsub("[Z]","Zaxis",feature.names.v7) 
+feature.names.v9<-gsub("mean","Mean",feature.names.v8)
+feature.names.v10<-gsub("std","StandardDeviation",feature.names.v9)
+feature.names.v11<-gsub("Mag","Magnitude",feature.names.v10) 
+feature.names.v12<-gsub("BodyAccelerometerJerk","BodyLinearAcceleration",feature.names.v11)
+feature.names.v13<-gsub("BodyGyroscopeJerk","BodyAngularVelocity",feature.names.v12)
 
 ##assigning the above vector the colnames of tidy data
-colnames(test.and.train.tidy.v2)[2:(length(feature.names)+1)]<-feature.names
+colnames(test.and.train.tidy.v2)[2:(length(feature.names.v13)+1)]<-feature.names.v13
 
 ##--------------------------------------------------------------------------------
 ##step 5:creating a second tidy data set with average of each measurement for each
 ##--------variable and each subject-----------------------------------------------
 ##--------------------------------------------------------------------------------
 
-tidy.data2<-as.data.frame(lapply(split(test.and.train.tidy.v2,list(test.and.train.tidy.v2$activityName,test.and.train.tidy.v2$subjectIndex)), 
+tidy.data2<-as.data.frame(
+  lapply(
+    split(test.and.train.tidy.v2,
+    list(test.and.train.tidy.v2$activityName,test.and.train.tidy.v2$subjectIndex)), 
                function(x) colMeans(x[,2:67])))
 
 ## the result obtained in previous step has subject & activity in columns and hence doing a transpose
 tidy.data2.v1<-as.data.frame(t(tidy.data2)) 
 
 ##updating the colnames to reflect they have the mean for each subject and activity
-colnames(tidy.data2.v1)<-paste("mean",colnames(tidy.data2.v1),sep=".")
+colnames(tidy.data2.v1)<-paste0("average",colnames(tidy.data2.v1),sep="")
 
 ##creating a new column from the rownames
-tidy.data2.v1$activityName.subjectIndex<-rownames(tidy.data2.v1)
+tidy.data2.v1$activityName<-unlist(strsplit(rownames(tidy.data2.v1),"\\."))[seq(from=1,to=length(rownames(tidy.data2.v1))*2,by=2)]
+tidy.data2.v1$SubjectIndex<-unlist(strsplit(rownames(tidy.data2.v1),"\\."))[seq(from=2,to=length(rownames(tidy.data2.v1))*2,by=2)]
 
 ##reordering columns
-tidy.data2.v2<-tidy.data2.v1[,c(67,1:66)]
+tidy.data2.v2<-tidy.data2.v1[,c(67,68,1:66)]
 
 ##exporting the final tidy data set
 write.table(tidy.data2.v2,"tidy_data_set2.txt",row.names=FALSE)
